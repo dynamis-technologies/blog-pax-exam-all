68b72c70bc60a6649edd10e67108485e2bbe4924
Clement Escoffier <clement.escoffier@gmail.com>
2013-02-17 16:53:03 +0100

    Provide the blog post support
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-blog-pax-exam-all
-=================
+Execute Pax Exam 3 tests in one run
+===================================
 
-Projects showing how to run Pax Exam 3 OSGi tests on Felix, Equinox and KF in one run
+Projects showing how to run Pax Exam 3 OSGi tests on Felix, Equinox and KF in one run. Explanations are on: (http://www.dynamis-technologies.com/blog/testing-osgi-applications-on-several-frameworks-with-pax-exam-3).
---
+++ it/src/test/java/org/sample/test/MyOSGiTest.java
@@ -0,0 +1,54 @@
+package org.sample.test;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.net.MalformedURLException;
+
+import static org.ops4j.pax.exam.CoreOptions.*;
+
+/**
+ * Let's test mostly nothing...
+ */
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class MyOSGiTest {
+
+    @Inject
+    BundleContext context;
+
+    @Configuration
+    public Option[] config() throws MalformedURLException {
+        // Reduce log level.
+        Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        root.setLevel(Level.INFO);
+
+        return options(
+                junitBundles(),
+                systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("WARN")
+        );
+    }
+
+    @Test
+    public void testSomething() {
+        // Dump OSGi Framework information
+        String vendor = context.getBundle(0).getHeaders().get(Constants.BUNDLE_VENDOR);
+        if (vendor == null) {
+            vendor = context.getBundle(0).getHeaders().get(Constants.BUNDLE_SYMBOLICNAME);
+        }
+        String version = context.getBundle(0).getHeaders().get(Constants.BUNDLE_VERSION);
+        System.out.println("OSGi Framework : " + vendor + " - " + version);
+    }
+}
